1e855b763a005d7704eb3068889345a5eb27a44d
Joao Victor <joaogmavictor@gmail.com>
2021-10-20 23:55:05 -0300

    Começando Skeleto projeto

--- Flutter/projeto_perguntas/lib/main.dart
+++ Flutter/projeto_perguntas/lib/main.dart
@@ -1,63 +1,43 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+main() => runApp(PerguntaAPP());
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+class PerguntaAppState extends State<PerguntaAPP> {
+  var perguntaSelecionada = 0;
+  void responder() {
     setState(() {
-      _counter++;
+      perguntaSelecionada++;
     });
+    print(perguntaSelecionada);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
+    final perguntas = [
+      'Qual é a sua cor favorita ?',
+      'Qaul é o seu animal favorito ?',
+    ];
+
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Perguntas'),
+          ),
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(perguntas[perguntaSelecionada]),
+              ElevatedButton(onPressed: responder, child: Text('Resposta 1')),
+              ElevatedButton(onPressed: responder, child: Text('Resposta 2')),
+              ElevatedButton(onPressed: responder, child: Text('Resposta 3'))
+            ],
+          )),
     );
+  }
+}
+
+class PerguntaAPP extends StatefulWidget {
+  PerguntaAppState createState() {
+    return PerguntaAppState();
   }
 }
